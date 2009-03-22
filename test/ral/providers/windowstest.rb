64ed0229c9f0a1a87780af002f0dc1891e078337
Joel Rosario <joel.r@.internal.directi.com>
2009-03-23 01:58:53 +0530

    All the utils classes went into Puppet::Util, and the tests were fixed.

--- test/ral/providers/windowstest.rb
+++ test/ral/providers/windowstest.rb
@@ -1,95 +1,101 @@
 require File.dirname(__FILE__) + '/../../../lib/puppet/util/windows_system.rb'
 
 module WindowsTest
+    include Puppet::Util::Windows
+
     class List
-	def initialize
-		@list = []
-	end
+        def initialize
+            @list = []
+        end
 
-	def clear
-		destroy
-		@list = []
-	end
+        def clear
+            destroy
+            @list = []
+        end
 
-	def register(item)
-		@list << item
-	end
+        def register(item)
+            @list << item
+        end
     end
 
     class Groups < List
-	def destroy
-		@list.each {|group|
-			begin
-				Windows::Group.delete(group)
-			rescue
-				puts "Group #{group} not found"
-			end
-		}
-	end
+        include Puppet::Util::Windows
+
+        def destroy
+            @list.each {|group|
+                begin
+                    Group.delete(group)
+                rescue
+                    puts "Group #{group} not found"
+                end
+            }
+        end
     end
 
     class Users < List
-	def destroy
-		@list.each {|user|
-			begin
-				Windows::User.delete(user)
-			rescue
-				puts "User #{user} not found"
-			end
-		}
-	end
+        include Puppet::Util::Windows
+
+        def destroy
+            @list.each {|user|
+                begin
+                    User.delete(user)
+                rescue
+                    puts "User #{user} not found"
+                end
+            }
+        end
     end
 
     def helper_users
-	@users = Users.new if @users == nil
-	@users
+        @users = Users.new if @users == nil
+        @users
     end
 
     def helper_groups
-	@groups = Groups.new if @groups == nil
-	@groups
+        @groups = Groups.new if @groups == nil
+        @groups
     end
 
     def clear
-	helper_groups.clear
-	helper_users.clear
+        helper_groups.clear
+        helper_users.clear
     end
 
     def register_group(name)
-	helper_groups.register name
+        helper_groups.register name
     end
 
     def register_user(name)
-	helper_users.register name
+        helper_users.register name
     end
 
     def mkuser(name, password = "1234567")
-	Windows::User.create(name, password) { register_user name }
+        User.create(name, password) { register_user name }
     end
 
     def mkgroup(name)
-	Windows::Group.create(name) { register_group name }
+        Group.create(name) { register_group name }
     end
 
     def mkusers(names)
-	names.collect {|name| mkuser name }
+        names.collect {|name| mkuser name }
     end
 
     def mkgroups(names)
-	names.collect {|name| mkgroup name }
+        names.collect {|name| mkgroup name }
     end
 
     def group(name)
-	Windows::Group.new(name)
+        Group.new(name)
     end
 
     def assert_no_missing_member(group, expected_members)
-	group.members.tap {|members|
-		expected_members.each {|member| assert(members.include?(member), "#{member} should be a member") }
-	}
+        group.members.tap {|members|
+            expected_members.each {|member| assert(members.include?(member), "#{member} should be a member") }
+        }
     end
 
     def teardown
-      clear
+        clear
     end
 end
