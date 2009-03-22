f3d18a0d1b511eaec36e3e932acf08d5fd37707f
Joel Rosario <joel.r@.internal.directi.com>
2009-03-23 01:58:53 +0530

    All the utils classes went into Puppet::Util, and the tests were fixed.

--- lib/puppet/util/windows_system.rb
+++ lib/puppet/util/windows_system.rb
@@ -3,8 +3,8 @@ if Facter.operatingsystem == "windows"
 	require 'Win32API'
 end
 
-module ADSI
-	def ADSI.connectable?(uri)
+module Puppet::Util::ADSI
+	def self.connectable?(uri)
 		begin
 			adsi_obj = WIN32OLE.connect(uri)
 			return adsi_obj != nil;
@@ -15,191 +15,201 @@ module ADSI
 	end
 end
 
-module Windows
-	class Resource
-		def Resource.uri(resource_name)
-			"#{Computer.resource_uri}/#{resource_name}"
-		end
-	end
+module Puppet::Util::Windows
+    include Puppet::Util::ADSI
 
-	class User
-		def initialize(username, native_adsi_obj = nil)
-			@username = username
-			@user = native_adsi_obj
-		end
+    class Resource
+        def Resource.uri(resource_name)
+            "#{Computer.resource_uri}/#{resource_name}"
+        end
+    end
 
-		def user
-			@user = WIN32OLE.connect(User.resource_uri(@username)) if @user == nil
-			return @user
-		end
+    class User
+        def initialize(username, native_adsi_obj = nil)
+            @username = username
+            @user = native_adsi_obj
+        end
 
-		def password_is?(password)
-			fLOGON32_LOGON_NETWORK_CLEARTEXT = 8
-			fLOGON32_PROVIDER_DEFAULT = 0
+        def user
+            @user = WIN32OLE.connect(User.resource_uri(@username)) if @user == nil
+            return @user
+        end
 
-			logon_user = Win32API.new("advapi32", "LogonUser", ['P', 'P', 'P', 'L', 'L', 'P'], 'L')
-			close_handle = Win32API.new("kernel32", "CloseHandle", ['P'], 'V')
+        def password_is?(password)
+            fLOGON32_LOGON_NETWORK_CLEARTEXT = 8
+            fLOGON32_PROVIDER_DEFAULT = 0
 
-			token = ' ' * 4
-			if logon_user.call(@username, "", password, fLOGON32_LOGON_NETWORK_CLEARTEXT, fLOGON32_PROVIDER_DEFAULT, token) == 1
-				close_handle.call(token.unpack('L')[0])
-				return true
-			end
+            logon_user = Win32API.new("advapi32", "LogonUser", ['P', 'P', 'P', 'L', 'L', 'P'], 'L')
+            close_handle = Win32API.new("kernel32", "CloseHandle", ['P'], 'V')
 
-			return false
-		end
+            token = ' ' * 4
+            if logon_user.call(@username, "", password, fLOGON32_LOGON_NETWORK_CLEARTEXT, fLOGON32_PROVIDER_DEFAULT, token) == 1
+                close_handle.call(token.unpack('L')[0])
+                return true
+            end
 
-		def add_flag(flag_name, value)
-			flag = 0
+            return false
+        end
 
-			begin
-				flag = user.Get(flag_name)
-			rescue
-			end
+        def add_flag(flag_name, value)
+            flag = 0
 
-			user.Put(flag_name, flag | value)
-			user.SetInfo
-		end
+            begin
+                flag = user.Get(flag_name)
+            rescue
+            end
 
-		def password=(password)
-			user.SetPassword(password)
-			user.SetInfo
+            user.Put(flag_name, flag | value)
+            user.SetInfo
+        end
 
-			fADS_UF_DONT_EXPIRE_PASSWD = 0x10000
-			add_flag("UserFlags", fADS_UF_DONT_EXPIRE_PASSWD)
-		end
+        def password=(password)
+            user.SetPassword(password)
+            user.SetInfo
 
-		def groups
-			groups = []
-			user.Groups.each {|group| groups << group.name }
-			return groups
-		end
+            fADS_UF_DONT_EXPIRE_PASSWD = 0x10000
+            add_flag("UserFlags", fADS_UF_DONT_EXPIRE_PASSWD)
+        end
 
-		def add_to_groups(group_names)
-			group_names.each {|name| Group.new(name).add_user(@username) } if group_names.length > 0
-		end
+        def groups
+            groups = []
+            user.Groups.each {|group| groups << group.name }
+            return groups
+        end
 
-		def remove_from_groups(group_names)
-			group_names.each {|name| Group.new(name).remove_user(@username) } if group_names.length > 0
-		end
+        def add_to_groups(group_names)
+            group_names.each {|name| Group.new(name).add_user(@username) } if group_names.length > 0
+        end
 
-		def set_groups(names, minimal = true)
-			return if names == nil || names.strip.length == 0
+        def remove_from_groups(group_names)
+            group_names.each {|name| Group.new(name).remove_user(@username) } if group_names.length > 0
+        end
 
-			names = names.strip.split(',')
-			current_groups = groups
+        def set_groups(names, minimal = true)
+            return if names == nil || names.strip.length == 0
 
-			names.find_all {|name| !current_groups.include?(name) }.tap {|names_to_add| add_to_groups(names_to_add) }
-			current_groups.find_all {|name| !names.include?(name) }.tap {|names_to_remove| remove_from_groups(names_to_remove) } if minimal == false
-		end
+            names = names.strip.split(',')
+            current_groups = groups
 
-		def User.resource_uri(username)
-			return "#{Resource.uri(username)},user"
-		end
+            names_to_add = names.find_all {|name| !current_groups.include?(name) }
+            add_to_groups(names_to_add)
 
-		def User.exists?(username)
-			return ADSI::connectable?(User.resource_uri(username))
-		end
+            names_to_remove = current_groups.find_all {|name| !names.include?(name) }
+            remove_from_groups(names_to_remove) if minimal == false
+        end
 
-		def User.create(username, password)
-			User.new(username, Computer.create("user", username)).tap {|user| user.password = password; yield user if block_given? }
-		end
+        def User.resource_uri(username)
+            return "#{Resource.uri(username)},user"
+        end
 
-		def User.delete(username)
-			Computer.delete("user", username)
-		end
-	end
+        def User.exists?(username)
+            return Puppet::Util::ADSI::connectable?(User.resource_uri(username))
+        end
 
-	class Group
-		def initialize(groupname, native_adsi_obj = nil)
-			@groupname = groupname
-			@group = native_adsi_obj
-		end
+        def User.create(username, password)
+            newuser = new(username, Computer.create("user", username))
+            newuser.password = password
+            yield newuser if block_given?
+            return newuser
+        end
 
-		def resource_uri
-			Group.resource_uri(@groupname)
-		end
+        def User.delete(username)
+            Computer.delete("user", username)
+        end
+    end
 
-		def Group.resource_uri(name)
-			"#{Resource.uri(name)},group"
-		end
+    class Group
+        def initialize(groupname, native_adsi_obj = nil)
+            @groupname = groupname
+            @group = native_adsi_obj
+        end
 
-		def group
-			@group = WIN32OLE.connect(resource_uri) if @group == nil
-			return @group
-		end
+        def resource_uri
+            Group.resource_uri(@groupname)
+        end
 
-		def add_user(username)
-			group.Add(User.resource_uri(username))
-			group.SetInfo
-		end
+        def Group.resource_uri(name)
+            "#{Resource.uri(name)},group"
+        end
 
-		def remove_user(username)
-			group.Remove(User.resource_uri(username))
-			group.SetInfo
-		end
+        def group
+            @group = WIN32OLE.connect(resource_uri) if @group == nil
+            return @group
+        end
 
-		def add_member(name)
-			group.Add(Resource.uri(name))
-			group.SetInfo
-		end
+        def add_user(username)
+            group.Add(User.resource_uri(username))
+            group.SetInfo
+        end
 
-		def remove_member(name)
-			group.Remove(Resource.uri(name))
-			group.SetInfo
-		end
+        def remove_user(username)
+            group.Remove(User.resource_uri(username))
+            group.SetInfo
+        end
 
-		def members
-			list = []
-			group.Members.each {|member| list << member.Name }
-			list
-		end
+        def add_member(name)
+            group.Add(Resource.uri(name))
+            group.SetInfo
+        end
 
-		def set_members(members)
-			return nil if members == nil || members.length == 0
+        def remove_member(name)
+            group.Remove(Resource.uri(name))
+            group.SetInfo
+        end
 
-			current_members = self.members
+        def members
+            list = []
+            group.Members.each {|member| list << member.Name }
+            list
+        end
 
-			members.inject([]) {|members_to_add, member| current_members.include?(member) ? members_to_add : members_to_add << member }.each {|member| add_member(member) }
-			current_members.inject([]) {|members_to_remove, member| members.include?(member) ? members_to_remove : members_to_remove << member }.each {|member| remove_member(member) }
-		end
+        def set_members(members)
+            return nil if members == nil || members.length == 0
 
-		def Group.create(name)
-			Windows::Group.new(name, Computer.create("group", name)).tap {|group| yield group if block_given? }
-		end
+            current_members = self.members
 
-		def Group.exists?(name)
-			return ADSI::connectable?(Group.resource_uri(name))
-		end
+            members.inject([]) {|members_to_add, member| current_members.include?(member) ? members_to_add : members_to_add << member }.each {|member| add_member(member) }
+            current_members.inject([]) {|members_to_remove, member| members.include?(member) ? members_to_remove : members_to_remove << member }.each {|member| remove_member(member) }
+        end
 
-		def Group.delete(name)
-			Computer.delete("group", name)
-		end
-	end
+        def Group.create(name)
+            newgroup = new(name, Computer.create("group", name))
+            yield newgroup if block_given?
+            return newgroup
+        end
 
-	class Computer
-		def Computer.name
-			name = " " * 128
-			size = "128"
-			Win32API.new('kernel32','GetComputerName',['P','P'],'I').call(name,size)
-			return name.unpack("A*")
-		end
+        def Group.exists?(name)
+            return Puppet::Util::ADSI::connectable?(Group.resource_uri(name))
+        end
 
-		def Computer.resource_uri
-			computer_name = Computer.name
-			return "WinNT://#{computer_name}"
-		end
+        def Group.delete(name)
+            Computer.delete("group", name)
+        end
+    end
 
-		def Computer.api
-			return WIN32OLE.connect(Computer.resource_uri)
-		end
+    class Computer
+        def Computer.name
+            name = " " * 128
+            size = "128"
+            Win32API.new('kernel32','GetComputerName',['P','P'],'I').call(name,size)
+            return name.unpack("A*")
+        end
 
-		def Computer.create(resource_type, name)
-			Computer.api.create(resource_type, name).SetInfo
-		end
+        def Computer.resource_uri
+            computer_name = Computer.name
+            return "WinNT://#{computer_name}"
+        end
 
-		def Computer.delete(resource_type, name)
-			Computer.api.Delete(resource_type, name)
-		end
-	end
+        def Computer.api
+            return WIN32OLE.connect(Computer.resource_uri)
+        end
+
+        def Computer.create(resource_type, name)
+            Computer.api.create(resource_type, name).SetInfo
+        end
+
+        def Computer.delete(resource_type, name)
+            Computer.api.Delete(resource_type, name)
+        end
+    end
 end
