7260a98c3c490945f371fc28fabef3e8eef94f0f
Joel Rosario <cirquitz@gmail.com>
2009-05-23 21:14:13 +0530

    Cleaned up users and groups providers for windows so that they don't instantiate a new instance of group each time a property is set. Now methods share a single instance of a group where possible.:

--- lib/puppet/provider/group/groupadd_win.rb
+++ lib/puppet/provider/group/groupadd_win.rb
@@ -6,24 +6,33 @@ Puppet::Type.type(:group).provide :groupadd_win do
 
     has_features :manages_members
 
+    def group
+        @group = Puppet::Util::Windows::Group.new(name) unless defined?(@group)
+        @group
+    end
+    
+    def name
+        @resource[:name]
+    end
+
     def members
-        Puppet::Util::Windows::Group.new(@resource[:name]).members
+        group.members
     end
 
     def members=(members)
-        Puppet::Util::Windows::Group.new(@resource[:name]).set_members(members)
+        group.set_members(members)
     end
 
     def create
-        group = Puppet::Util::Windows::Group.create(@resource[:name])
-        group.set_members(@resource[:members])
+        @group = Puppet::Util::Windows::Group.create(name)
+        @group.set_members(@resource[:members])
     end
 
     def exists?
-        Puppet::Util::Windows::Group.exists?(@resource[:name])
+        Puppet::Util::Windows::Group.exists?(name)
     end
 
     def delete
-        Puppet::Util::Windows::Group.delete(@resource[:name])
+        Puppet::Util::Windows::Group.delete(name)
     end
 end
